f2ace4cfcdfa78c7b434c56833a2ad5aa8cad7ed
Bill Mårtensson <bill@magictechnology.se>
2022-10-17 09:25:23 +0200

    Initial Commit

---
+++ PIA11Firebase/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PIA11Firebase
+//
+//  Created by Bill Martensson on 2022-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PIA11Firebase/PIA11FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIA11FirebaseApp.swift
+//  PIA11Firebase
+//
+//  Created by Bill Martensson on 2022-10-17.
+//
+
+import SwiftUI
+
+@main
+struct PIA11FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
